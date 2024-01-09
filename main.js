5bb3cf9b57587d3c4faaf8971a554e317c5701db
cleverbeetle <122474779+cleverbeetle@users.noreply.github.com>
2024-01-09 14:52:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function clicks() {
     $('#div2').addClass("fadeinv2");
         
     var typed = new Typed('.katakata', {
-        strings: ["Semangat ya kerjanya", "Jangan dijadiin beban kedirimu", "Apa yang akan terjadi besok", "itu tetap rahasia tuhan","tugasmu hanyalah bersyukur dihari ini", "jangan terlalu stress ya", "hubungi aku kalau ada apa-apa ya", "im alwyas with you", "AKU BANGGA SAMA KAMU!", "I LOVE YOU TO THE MOON AND BACK"],
+        strings: ["Semangat ya buat hari ini", "Jangan dijadiin beban kedirimu", "Apa yang akan terjadi besok", "itu tetap rahasia tuhan", "tugasmu hanyalah bersyukur dihari ini", "jangan terlalu stress ya", "hubungi aku kalau ada apa-apa ya", "im alwyas with you", "AKU BANGGA SAMA KAMU!", "I LOVE YOU TO THE MOON AND BACK"],
         typeSpeed: 70
     });
 }
