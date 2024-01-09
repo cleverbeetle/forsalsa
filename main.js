77a603a7bdc0c1d07ea211578adb7092089cfeba
cleverbeetle <122474779+cleverbeetle@users.noreply.github.com>
2024-01-09 14:56:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ function clicks() {
         
     var typed = new Typed('.katakata', {
         strings: ["Semangat ya buat hari ini", "Jangan dijadiin beban kedirimu", "Apa yang akan terjadi besok", "itu tetap rahasia tuhan", "tugasmu hanyalah bersyukur dihari ini", "jangan terlalu stress ya", "hubungi aku kalau ada apa-apa ya", "im alwyas with you", "AKU BANGGA SAMA KAMU!", "I LOVE YOU TO THE MOON AND BACK"],
-        typeSpeed: 120
+        typeSpeed: 20
     });
 }
